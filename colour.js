2799f53dc6f10a457777a6d08b04fa794f42b4b0
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-03-27 12:55:12 +0000

    first commit

---
+++ colour.js
@@ -0,0 +1,18 @@
+"strict";
+
+const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let hexColour = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColour += colors[getRandomNumber()];
+  }
+
+  document.body.style.backgroundColor = hexColour;
+  color.textContent = hexColour;
+});
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
